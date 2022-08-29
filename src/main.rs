80f84d548b55fdaa5962c213149dd0459f0b98da
Christian Westrom <c.westrom@westrom.xyz>
2022-08-29 13:10:11 -0400

    a bit of refactoring

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,12 @@ use rust_music_theory::{
     note::PitchClass,
 };
 
-struct DisplayableChord(Chord);
+struct DisplayableChord<'a>(pub &'a Chord);
 
-impl Display for DisplayableChord {
+impl<'a> Display for DisplayableChord<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{} {}", self.0.root, self.0.quality))
+        f.write_fmt(format_args!("{}-{}", self.0.root, self.0.quality))
     }
-}
-
-fn chord_string(chord: &Chord) -> String {
-    format!("{}-{}", chord.root, chord.quality)
 }
 
 #[derive(Debug)]
@@ -28,10 +24,13 @@ struct TwoFiveProgression {
 impl TwoFiveProgression {
     fn new(scale_degree: ScaleDegree) -> Self {
         let pitch = scale_degree.pitch;
-        let offset = 12 - scale_degree.position.semitone_count;
-        let root_pitch = PitchClass::from_interval(pitch, Interval::from_semitone((offset + 0) % 12).unwrap());
-        let two_pitch  = PitchClass::from_interval(pitch, Interval::from_semitone((offset + 2) % 12).unwrap());
-        let five_pitch = PitchClass::from_interval(pitch, Interval::from_semitone((offset + 7) % 12).unwrap());
+        let offset = 12 - scale_degree.interval_from_root.semitone_count;
+        let root_pitch =
+            PitchClass::from_interval(pitch, Interval::from_semitone((offset + 0) % 12).unwrap());
+        let two_pitch =
+            PitchClass::from_interval(pitch, Interval::from_semitone((offset + 2) % 12).unwrap());
+        let five_pitch =
+            PitchClass::from_interval(pitch, Interval::from_semitone((offset + 7) % 12).unwrap());
         Self {
             root_chord: Chord::new(root_pitch, Quality::Major, Number::Triad),
             two_chord: Chord::new(two_pitch, Quality::Minor, Number::Triad),
@@ -42,39 +41,40 @@ impl TwoFiveProgression {
 
 impl Display for TwoFiveProgression {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let root_chord_str = chord_string(&self.root_chord);
-        let two_chord_str = chord_string(&self.two_chord);
-        let five_chord_str = chord_string(&self.five_chord);
         f.write_fmt(format_args!(
             "{} {} {}",
-            two_chord_str, five_chord_str, root_chord_str
+            DisplayableChord(&self.two_chord),
+            DisplayableChord(&self.five_chord),
+            DisplayableChord(&self.root_chord)
         ))
     }
 }
 
 struct ScaleDegree {
     pitch: PitchClass,
-    position: Interval,
+    interval_from_root: Interval,
 }
 
 fn main() {
-    let root = ScaleDegree {
-        pitch: PitchClass::from_str("C").unwrap(),
-        position: Interval::from_semitone(0).unwrap(),
+    use std::env;
+
+    let args: Vec<String> = env::args().collect();
+
+    let pitch = &args[1];
+    let interval = &args[2].parse::<u8>().unwrap();
+
+    let program_name = "music-calculator";
+
+    println!("ii-V-I!");
+    println!("Usage: {} [PITCH] [INTERVAL]", program_name);
+    println!();
+    let scaledeg = ScaleDegree {
+        pitch: PitchClass::from_str(pitch).unwrap(),
+        interval_from_root: Interval::from_semitone(*interval).unwrap(),
     };
-    println!("ii-V-I");
-    dbg!(&root.pitch);
-    println!("{}", TwoFiveProgression::new(root));
-    let two = ScaleDegree {
-        pitch: PitchClass::from_str("D").unwrap(),
-        position: Interval::from_semitone(2).unwrap(),
-    };
-    dbg!(&two.pitch);
-    println!("{}", TwoFiveProgression::new(two));
-    let five = ScaleDegree {
-        pitch: PitchClass::from_str("G").unwrap(),
-        position: Interval::from_semitone(7).unwrap(),
-    };
-    dbg!(&five.pitch);
-    println!("{}", TwoFiveProgression::new(five));
+    println!(
+        "Given that {} is {} semitones from the root",
+        scaledeg.pitch, scaledeg.interval_from_root.semitone_count
+    );
+    println!("{}", TwoFiveProgression::new(scaledeg));
 }
